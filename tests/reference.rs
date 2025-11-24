bdc6e0cf19c074c8dbc2a05d4810689fe3a031f9
Jonathan Behrens <fintelia@users.noreply.github.com>
2025-11-23 18:29:31 -0800

    Use TIFF for floating point reference images (#22)

--- tests/reference.rs
+++ tests/reference.rs
@@ -1,4 +1,4 @@
-use image::{ColorType, DynamicImage};
+use image::ColorType;
 use walkdir::WalkDir;
 
 #[test]
@@ -7,12 +7,20 @@ fn test_decoding() {
 
     for entry in WalkDir::new("tests/images") {
         let entry = entry.unwrap();
-        if !entry.file_type().is_file() || entry.path().extension().unwrap() == "png" {
+        if !entry.file_type().is_file()
+            || entry.path().extension().unwrap() == "png"
+            || entry.path().extension().unwrap() == "tiff"
+        {
             continue;
         }
 
-        let img = to_png_compatible_color_type(image::open(entry.path()).unwrap());
-        let reference = image::open(entry.path().with_extension("png")).unwrap();
+        let img = image::open(entry.path()).unwrap();
+        let reference = match img.color() {
+            ColorType::Rgb32F | ColorType::Rgba32F => {
+                image::open(entry.path().with_extension("tiff")).unwrap()
+            }
+            _ => image::open(entry.path().with_extension("png")).unwrap(),
+        };
 
         assert_eq!(
             img,
@@ -20,14 +28,5 @@ fn test_decoding() {
             "Image {} does not match reference",
             entry.path().display()
         );
-    }
-}
-
-/// PNG doesn't support 32-bit float color types, so convert them to 16-bit.
-fn to_png_compatible_color_type(image: DynamicImage) -> DynamicImage {
-    match image.color() {
-        ColorType::Rgb32F => image.to_rgb16().into(),
-        ColorType::Rgba32F => image.to_rgba16().into(),
-        _ => image,
     }
 }
